5b13e01242324e55671b13de2909013f7657e25a
Marouen <9755388+Marujah@users.noreply.github.com>
2023-11-19 19:10:46 +0100

    Delete mtah directory

--- mtah/content.js
+++
@@ -1,7 +0,0 @@
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'changeBackground') {
-    // Change the background color here
-    document.body.style.backgroundColor = '#2eaeff';
-  }
-});
